bc947a008bc96356df4f71731e4eed432317fb99
Paul Chheang <phalcheat@gmail.com>
2024-07-30 18:45:09 -0400

    Output a list of dummy events to the EventsPage

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  { id: "E1", title: "Event 1" },
+  { id: "E2", title: "Event 2" },
+  { id: "E3", title: "Event 3" },
+];
+
 export default function EventsPage() {
-  return <h1>The Events</h1>;
+  return (
+    <>
+      <h1>The Events</h1>
+      <ul>
+        {EVENTS.map((e) => (
+          <li key={e.id}>
+            <Link to={`/events/${e.id}`}>{e.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
