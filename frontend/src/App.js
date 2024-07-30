955ba4e09a6c1daf16368dddcd9978f12b747702
Paul Chheang <phalcheat@gmail.com>
2024-07-30 18:21:59 -0400

    Added pages and setup routes and added Navbar to all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,30 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import NewEventPage from "./pages/NewEventPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./components/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/events", element: <EventsPage /> },
+      { path: "/events/:eventId", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ frontend/src/components/RootLayout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+import MainNavigation from "./MainNavigation";
+
+export default function RootLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <Outlet />
+    </>
+  );
+}
