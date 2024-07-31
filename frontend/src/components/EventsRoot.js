5fe597cea62deee0f5cfa43c9f8d4c968e54e29a
Paul Chheang <phalcheat@gmail.com>
2024-07-30 20:29:19 -0400

    Simplied some file names

--- frontend/src/components/EventsRoot.js
+++ frontend/src/components/EventsRoot.js
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import EventsNavigation from "./EventsNavigation";
 
-export default function EventsLayout() {
+export default function EventsRootLayout() {
   return (
     <>
       <EventsNavigation />
